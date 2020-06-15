235ff1642ab4709adb002202b5f48ba1ec0998bb
zak4132 <ryosei.s.0630@gmail.com>
2020-06-15 17:03:57 +0900

    アンケート内容の追加

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ const server = http
               secondItem: 'パン'
             })
           );
+        } else if (req.url === '/enquetes/sushi-pizza') {
+          res.write(pug.renderFile('./form.pug', {
+            path: req.url,
+            firstItem: '寿司',
+            secondItem: 'ピザ'
+          }));
         }
         res.end();
         break;
